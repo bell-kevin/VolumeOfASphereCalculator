be920184ebd11706653174b09c2c48efe2343423
Kevin Bell <bellKevin@pm.me>
2022-05-17 02:34:18 +0000

    Update VolumeOfASphereCalculator.java

--- VolumeOfASphereCalculator/src/volumeofaspherecalculator/VolumeOfASphereCalculator.java
+++ VolumeOfASphereCalculator/src/volumeofaspherecalculator/VolumeOfASphereCalculator.java
@@ -4,7 +4,7 @@ public class VolumeOfASphereCalculator {
     public static void main(String[] args) {
         Scanner computerKeyboardInput = new Scanner(System.in);
         double diameter;
-        System.out.print("Enter the diameter of the sphere: ");
+        System.out.print("Enter a diameter of a sphere: ");
         diameter = computerKeyboardInput.nextDouble();
         double volume = Math.PI / 6.0 * diameter * diameter * diameter;
         System.out.println("Sphere volume in cubic cm = " + volume);
